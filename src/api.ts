dcf02a641ce9d9275070fbbdd8beb2cfcc0dd1ba
Joel Rocha <joelxr@gmail.com>
2022-10-01 22:55:08 -0300

    .

--- src/api.ts
+++ src/api.ts
@@ -14,38 +14,37 @@ const port: number = Number(process.env.PORT) || 8091
 app.use(bodyParser.json())
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 app.use(bodyParser.text({ type: 'text/html' }))
-app.use(cors())
 
-app.use((req: any, res: any, next: any) => {
-  if (req.body && typeof req.body === 'object') {
-    req.body = humps.decamelizeKeys(req.body)
-  }
-
-  if (req.query && typeof req.query === 'object') {
-    req.query = humps.decamelizeKeys(req.query)
-  }
-
-  const sendJson = res.json
-  res.json = (data: any) => {
-    return sendJson.call(res, humps.camelizeKeys(data))
-  }
-
-  next()
-})
-
-app.use((err: any, req: any, res: any, next: any) => {
-  res.status(500).json({ message: err.message })
-})
-
-app.use(async (req: any, res: any, next: any) => {
-  const apiKey = req.headers['api-key']
-
-  if (!(await isValidApiKey(apiKey))) {
-    return res.status(401).json({ message: 'Invalid API Key' })
-  }
-
-  next()
-})
+//app.use((req: any, res: any, next: any) => {
+//  if (req.body && typeof req.body === 'object') {
+//    req.body = humps.decamelizeKeys(req.body)
+//  }
+//
+//  if (req.query && typeof req.query === 'object') {
+//    req.query = humps.decamelizeKeys(req.query)
+//  }
+//
+//  const sendJson = res.json
+//  res.json = (data: any) => {
+//    return sendJson.call(res, humps.camelizeKeys(data))
+//  }
+//
+//  next()
+//})
+//
+//app.use((err: any, req: any, res: any, next: any) => {
+//  res.status(500).json({ message: err.message })
+//})
+//
+//app.use(async (req: any, res: any, next: any) => {
+//  const apiKey = req.headers['api-key']
+//
+//  if (!(await isValidApiKey(apiKey))) {
+//    return res.status(401).json({ message: 'Invalid API Key' })
+//  }
+//
+//  next()
+//})
 
 app.get('/', async (req, res) => {
   res.send(`Hello, World!`)
@@ -56,6 +55,6 @@ app.use('/products', products)
 app.use('/product-tags', productTags)
 app.use('/expenses', expenses)
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Service is listening to port ${port}...`)
 })
