806c002f05191f65ddf9395db67dc21bb1d79615
Joel Rocha <joelxr@gmail.com>
2022-10-01 22:53:15 -0300

    .

--- src/api.ts
+++ src/api.ts
@@ -9,7 +9,7 @@ import expenses from './expenses'
 import { isValidApiKey } from './auth'
 
 const app: Application = express()
-const port: number = Number(process.env.PORT) | 8091
+const port: number = Number(process.env.PORT) || 8091
 
 app.use(bodyParser.json())
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
